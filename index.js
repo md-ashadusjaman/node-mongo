48f626df433f1d44d8457612c5cd985edd5cadb7
Asad <asad@Asads-MacBook-Pro.local>
2021-06-03 14:59:23 +0600

    Learning how to create dynamic api

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ const app = express()
 
 
 app.get('/', (req, res) => {
-    res.send('hello world')
+    const fruit = {
+        product:"banana",
+        price:45
+    }
+    res.send(fruit);
+  });
+
+
+  app.get('/fruits/banana', (req, res) => {
+      res.send({fruits:'banana', quantity:200, price:800});
+  })
+
+  //crating dynamic api
+
+  const users = ["asad","nobel", "karim", "rahim"]
+  app.get ('/users/:id', (req,res) =>{
+      const id = req.params.id;
+      //console.log(req.params.id)
+        const name = users[id]
+        res.send({name,id});
+
   })
   
-  app.listen(3000, )
+  app.listen(3000, () => console.log("listening the calling port"))
